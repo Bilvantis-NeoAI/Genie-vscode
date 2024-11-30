bd87fa5c4075477d6781c24ba84baa1e127fe142
RahulRamagiri09 <rahul.ramagiri@bilvantis.io>
2024-11-30 11:14:35 +0530

    Removed 'out' folder and added it to .gitignore

--- genie-vscode/out/auth/apiHeaders.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAuthHeaders = getAuthHeaders;
-function getAuthHeaders(authToken) {
-    return {
-        Authorization: `Bearer ${authToken}`,
-    };
-}
-//# sourceMappingURL=apiHeaders.js.map
