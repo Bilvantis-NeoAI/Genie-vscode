228f699b9714ae9e8dd78c647e00b8c79efdd1dd
RahulRamagiri09 <rahul.ramagiri@bilvantis.io>
2025-01-02 10:44:54 +0530

    removed shortcut keys

--- genie-vscode/src/commands/sidebarCommandRegister/GenieCommandsProvider.ts
+++ genie-vscode/src/commands/sidebarCommandRegister/GenieCommandsProvider.ts
@@ -21,34 +21,37 @@ export class GenieCommandsProvider implements vscode.TreeDataProvider<GenieComma
       ]);
     } else if (element.label === "Assistant") {
       return Promise.resolve([
-        new GenieCommand("Add Docstring", "extension.addDocstrings", "Ctrl+Alt+D", "Cmd+Ctrl+D"),
-        new GenieCommand("Add Error Handler", "extension.errorHandling", "Ctrl+Alt+E", "Cmd+Ctrl+E"),
-        new GenieCommand("Add Logging", "extension.addLogging", "Ctrl+Alt+L", "Cmd+Ctrl+L"),
-        new GenieCommand("Code Generation", "extension.codeGeneration", "Ctrl+Alt+G", "Cmd+Ctrl+G"),
-        new GenieCommand("Comment Code", "extension.addComments", "Ctrl+Alt+C", "Cmd+Ctrl+C"),
-        new GenieCommand("Explain Code", "extension.explainCode", "Ctrl+Alt+X", "Cmd+Ctrl+X"),
-        new GenieCommand("Refactor Code", "extension.refactorCode", "Ctrl+Alt+R", "Cmd+Ctrl+R"),
-        new GenieCommand("Unit Test Code", "extension.unittestCode", "Ctrl+Alt+U", "Cmd+Ctrl+U")
+        new GenieCommand("Add Docstring", "extension.addDocstrings", 'file-code'),
+        new GenieCommand("Add Error Handler", "extension.errorHandling", 'warning'),
+        new GenieCommand("Add Logging", "extension.addLogging", 'debug'),
+        new GenieCommand("Code Generation", "extension.codeGeneration", 'rocket'),
+        new GenieCommand("Comment Code", "extension.addComments", 'comment'),
+        new GenieCommand("Explain Code", "extension.explainCode", 'info'),
+        new GenieCommand("Refactor Code", "extension.refactorCode", 'gear'),
+        new GenieCommand("Unit Test Code", "extension.unittestCode", 'check')
       ]);
     } else if (element.label === "Review") {
       return Promise.resolve([
-        new GenieCommand("Code Overall Review", "extension.reviewOverall", "Ctrl+Shift+O", "Cmd+Shift+O"),
-        new GenieCommand("Code Review", "extension.reviewCode", "Ctrl+Shift+R", "Cmd+Shift+R"),
-        new GenieCommand("Tech Depth Review", "extension.reviewTechDepth", "Ctrl+Shift+C", "Cmd+Shift+C"),
-        new GenieCommand("Org Std Review", "extension.reviewOrgStd", "Ctrl+Shift+G", "Cmd+Shift+G"),
-        new GenieCommand("Owasp Review", "extension.reviewOwasp", "Ctrl+Shift+W", "Cmd+Shift+W"),
-        new GenieCommand("Performance Review", "extension.reviewPerformance", "Ctrl+Shift+P", "Cmd+Shift+P"),
-        new GenieCommand("Security Review", "extension.reviewSecurity", "Ctrl+Shift+S", "Cmd+Shift+S"),
-        new GenieCommand("Syntax Review", "extension.reviewSyntax", "Ctrl+Shift+Y", "Cmd+Shift+Y"),
+        new GenieCommand("Code Overall Review", "extension.reviewOverall", 'file-code'),
+        new GenieCommand("Code Review", "extension.reviewCode", 'file-text'),
+        new GenieCommand("Tech Depth Review", "extension.reviewTechDepth", 'flame'),
+        new GenieCommand("Org Std Review", "extension.reviewOrgStd", 'organization'),
+        new GenieCommand("Owasp Review", "extension.reviewOwasp", 'shield'),
+        new GenieCommand("Performance Review", "extension.reviewPerformance", 'pulse'),
+        new GenieCommand("Security Review", "extension.reviewSecurity", 'lock'),
+        new GenieCommand("Syntax Review", "extension.reviewSyntax", 'checklist'),
+        
       ]);
     } else if (element.label === "Git - Knowledge Base") {
       return Promise.resolve([
-        new GenieCommand("Explain", "extension.explainGitKB", "Ctrl+Shift+A", "Cmd+Shift+A"),
-        new GenieCommand("Get Code", "extension.getCodeGitKB", "Ctrl+Shift+B", "Cmd+Shift+B")
+        new GenieCommand("Explain", "extension.explainGitKB", 'comment'),
+        new GenieCommand("Get Code", "extension.getCodeGitKB", 'git-branch'),
+        
       ]);
     } else if (element.label === "Knowledge Base") {
       return Promise.resolve([
-        new GenieCommand("Get Response From KB", "extension.knowledgeBaseQueAns", "Ctrl+Shift+K", "Cmd+Shift+K"),
+        new GenieCommand("Get Response From KB", "extension.knowledgeBaseQueAns", 'search'),
+
       ]);
     }
 
@@ -69,16 +72,14 @@ class GenieCategory extends vscode.TreeItem {
 }
 
 class GenieCommand extends vscode.TreeItem {
-  constructor(label: string, command: string, ctrlShortcut: string, cmdShortcut: string) {
+  constructor(label: string, command: string, iconName: string) {
     super(label, vscode.TreeItemCollapsibleState.None);
-    const isMac = process.platform === "darwin"; // Detect macOS
-    const shortcut = isMac ? cmdShortcut : ctrlShortcut;
-
     this.command = {
       command,
       title: label,
     };
-    this.tooltip = `${label} (${shortcut})`; // Show shortcut in the tooltip
-    this.description = shortcut; // Show shortcut as the description
+    this.tooltip = `${label}`; // Show shortcut in the tooltip
+    this.description = ''; // Show shortcut as the description
+    this.iconPath = new vscode.ThemeIcon(iconName);
   }
 }
