123f88f2fecdcc80b8b1e2136e23dd64d97e32b1
RahulRamagiri09 <rahul.ramagiri@bilvantis.io>
2025-02-07 17:15:34 +0530

    hsbc backend url is added in config file

--- genie-vscode/src/auth/config.ts
+++ genie-vscode/src/auth/config.ts
@@ -1,4 +1,5 @@
-export let BASE_API='http://34.46.36.105:3000/app';
+// export let BASE_API='http://34.46.36.105:3000/app';
+export let BASE_API='http://mongo.hsbc-12432649-codephase-dev.dev.gcp.cloud.in.hsbc.:3000/app';
 export let ANSWER_CONFIG="chroma";
 
 export const exchangeUrl= (userUrl: string) =>
