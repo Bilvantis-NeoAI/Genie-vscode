a4b54e2e575302e65bf60c300c571bd06b1fd2ac
RahulRamagiri09 <rahul.ramagiri@bilvantis.io>
2025-02-24 09:51:31 +0530

    added sidebar condition formate

--- genie-vscode/src/auth/config.ts
+++ genie-vscode/src/auth/config.ts
@@ -1,23 +1,21 @@
 import * as vscode from 'vscode';
 
-let BASE_API = ""; // Default API URL
+let BASE_API = "";
+
+export const review_config = 'HSBC';
 export const ANSWER_CONFIG = "chroma";
 
-// Getter function for BASE_API
 export const getBaseApi = (): string => BASE_API;
 
-// Setter function to update BASE_API safely
 export const setBaseApi = (userUrl: string): void => {
     BASE_API = userUrl;
 };
 
-// Function to update BASE_API and store it in global state
 export const exchangeUrl = (context: vscode.ExtensionContext, userUrl: string) => {
     setBaseApi(userUrl);
     context.globalState.update('USER_BASE_API', userUrl);
 };
 
-// Function to load BASE_API from global state when extension starts
 export const loadBaseApi = (context: vscode.ExtensionContext) => {
     const storedBaseApi = context.globalState.get<string>('USER_BASE_API');
     if (storedBaseApi) {
