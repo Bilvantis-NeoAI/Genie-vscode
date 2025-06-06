b39f6e459845c7a309558c2db5d22764977aa788
RahulRamagiri09 <rahul.ramagiri@bilvantis.io>
2025-06-06 10:03:29 +0530

    added vsix package

--- genie-vscode/src/utils/api/KBAPI.ts
+++ genie-vscode/src/utils/api/KBAPI.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuthHeaders } from "../../auth/apiHeaders";
 import { getKbBaseApi } from "../../auth/config";
 
-
+// const test = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJyYWh1bDk3QGdtYWlsLmNvbSIsInVzZXJJZCI6IjY4Mzk3NjhiMDFhZmI0YWEwMDI2NjE2MCIsImV4cCI6MTc1MTUyMjgyM30.Wzk3cpAYF5ObzDhCtuoYOTDRKiIX8wAkevtUF9sX7gE";
 export async function knowledgeBaseQA(query: string, session_id: string, dbquery: boolean, partition_name: string, partition_value: string, authToken: string, options?: { signal?: AbortSignal }): Promise<any> {
     const response = await axios.post(
         `${getKbBaseApi()}/process-query`,
